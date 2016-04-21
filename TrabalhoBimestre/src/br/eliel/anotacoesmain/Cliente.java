82b651a5c934fb709e4274c3b140ea826e243157
Eliel <elielmd>
2016-04-21 11:38:04 -0300

    V1.6 - Adequando os atibutos da classe cliente, e o a classe abstrata SqlGen!

--- TrabalhoBimestre/src/br/eliel/anotacoesmain/Cliente.java
+++ TrabalhoBimestre/src/br/eliel/anotacoesmain/Cliente.java
@@ -18,6 +18,38 @@ public class Cliente {
 	@Coluna(telefone="UsTelefone", tamanho=20)
 	private String telefone;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 	public Cliente() {
 		this(0, null);
 	}
