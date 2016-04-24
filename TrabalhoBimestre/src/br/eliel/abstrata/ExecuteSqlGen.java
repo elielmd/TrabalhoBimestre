93c459e1bb9f897f177b93efd3affb06ca0449c4
Eliel <elielmd>
2016-04-24 11:33:35 -0300

    V1.7 Ajustando a o metodo getCreateTable!

--- TrabalhoBimestre/src/br/eliel/abstrata/ExecuteSqlGen.java
+++ TrabalhoBimestre/src/br/eliel/abstrata/ExecuteSqlGen.java
@@ -27,6 +27,48 @@ public abstract class ExecuteSqlGen {
 			sb.append("CREATE TABLE ").append(nameTable).append(" (");
 
 			Field[] attributes = cl.getDeclaredFields();
+			
+			for (int i = 0; i < attributes.length; i++) {
+                Field field = attributes[i];
+
+                String nameColumn;
+                String typeColumn = null;
+
+                if (field.isAnnotationPresent(Coluna.class)) {
+                    Coluna annotationColumn = field.getAnnotation(Coluna.class);
+
+                    if (annotationColumn.nome().isEmpty()) {
+                        nameColumn = field.getName().toUpperCase();
+                    } else {
+                        nameColumn = annotationColumn.nome();
+                    }
+                } else {
+                    nameColumn = field.getName().toUpperCase();
+                }
+
+                Class<?> typeParemetros = field.getType();
+
+                if (typeParemetros.equals(String.class)) {
+                    if (field.getAnnotation(Coluna.class).tamanho() > -1) {
+                        typeColumn = "VARCHAR(" + field.getAnnotation(Coluna.class).tamanho() + ")";
+                    } else {
+                        typeColumn = "VARCHAR(100)";
+                    }
+                } else if (typeParemetros.equals(int.class)){
+                    if (field.getAnnotation(Coluna.class).pk() == true) {
+                        typeColumn = "INT NOT NULL";
+                    } else {
+                        typeColumn = "INT";
+                    }
+                } else if (typeParemetros.isEnum()) {
+                    typeColumn = "INT";
+                }
+
+                if (i > 0) sb.append(",");
+
+                sb.append("\n\t").append(nameColumn).append(" ").append(typeColumn);
+			}
 		}
 	}
 }
+			
