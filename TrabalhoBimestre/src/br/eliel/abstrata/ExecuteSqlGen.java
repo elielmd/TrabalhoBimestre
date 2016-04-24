d54c429e07998bf01d0a9e33206e52e2d6ecf2ea
Eliel <elielmd>
2016-04-24 19:52:23 -0300

    V2.9 - Desenvolvendo o metodo getSqlSelectAll!

--- TrabalhoBimestre/src/br/eliel/abstrata/ExecuteSqlGen.java
+++ TrabalhoBimestre/src/br/eliel/abstrata/ExecuteSqlGen.java
@@ -23,11 +23,8 @@ public class ExecuteSqlGen extends SqlGen {
 		System.out.println(strCreateTable);
 		String strDropTable = getDropTable(con, cliente);
 		System.out.println(strDropTable);
-		System.out.println("BATATA1");
 		PreparedStatement strGetSqlInsert = getSqlInsert(con, cliente);
-		System.out.println("BATATA2");
 		System.out.println(strGetSqlInsert);
-		System.out.println("BATATA3");
 		PreparedStatement strGetSqlSelectAll = getSqlSelectAll(con, cliente);
 		System.out.println(strGetSqlSelectAll);
 
@@ -176,102 +173,78 @@ public class ExecuteSqlGen extends SqlGen {
 	@Override
 	protected PreparedStatement getSqlInsert(Connection con, Object obj) {
 		Class<?> cl = obj.getClass();
-		StringBuilder sb = new StringBuilder();
-		String nomeTabela;
-		System.out.println("TESTE1");
-		if (cl.isAnnotationPresent(Tabela.class)) {
-			Tabela table = cl.getAnnotation(Tabela.class);
-			nomeTabela = table.value();
-		} else {
-			nomeTabela = cl.getSimpleName().toUpperCase();
-		}
-		System.out.println("TESTE2");
-		sb.append("INSERT INTO ").append(nomeTabela).append(" (");
+        StringBuilder sb = new StringBuilder();
+        String nomeTabela;
 
-		Field[] atributos = cl.getDeclaredFields();
-		System.out.println("TESTE3");
-		for (int i = 0; i < atributos.length; i++) {
-			Field field = atributos[i];
-			String nomeColuna;
-			if (field.isAnnotationPresent(Coluna.class)) {
-				Coluna column = field.getAnnotation(Coluna.class);
-				if (column.nome().isEmpty()) {
-					nomeColuna = field.getName().toUpperCase();
-				} else {
-					nomeColuna = column.nome();
-				}
-			} else {
-				nomeColuna = field.getName().toUpperCase();
-			}
-			if (i > 0) {
-				sb.append(", ");
-			}
-			sb.append(nomeColuna);
-		}
-		System.out.println("TESTE4");
-		sb.append(") VALUES (");
-		for (int i = 0; i < atributos.length; i++) {
-			if (i > 0)
-				sb.append(", ");
+        if (cl.isAnnotationPresent(Tabela.class)) {
+            Tabela table = cl.getAnnotation(Tabela.class);
+            nomeTabela = table.value();
+        } else {
+        	nomeTabela = cl.getSimpleName().toUpperCase();
+        }
 
-			sb.append("?");
-		}
-		System.out.println("TESTE5");
-		sb.append(")");
-		String add = sb.toString();
-		//System.out.println(add);
-		
-		PreparedStatement ps = null;
-		System.out.println(ps);
+        sb.append("INSERT INTO ").append(nomeTabela).append(" (");
 
-		try {
-			ps = con.prepareStatement(add);
-			System.out.println(ps);
-			for (int i = 0; i < atributos.length; i++) {
-				Field field = atributos[i];
-				Object type = field.getType();
-				field.setAccessible(true);
-				
-				if (type.equals(int.class)) {
-					ps.setInt(i + 1, field.getInt(obj));
-				} else if (type.equals(String.class)) {
-					ps.setString(i + 1, String.valueOf(field.get(obj)));
-				} else if (field.getType().isEnum()) {
-					Object value = field.get(obj);
-					Method metodo = value.getClass().getMethod("ordinal");
-					ps.setInt(i + 1, (Integer) metodo.invoke(value, null));
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
+        Field[] atributos = cl.getDeclaredFields();
 
-		return ps;
+        for (int i = 0; i < atributos.length; i++) {
+            Field field = atributos[i];
+            String nomeColuna;
 
+            if (field.isAnnotationPresent(Coluna.class)) {
+            	Coluna column = field.getAnnotation(Coluna.class);
+                if (column.nome().isEmpty()) {
+                	nomeColuna = field.getName().toUpperCase();
+                } else {
+                	nomeColuna = column.nome();
+                }
+            } else {
+            	nomeColuna = field.getName().toUpperCase();
+            }
+
+            if (i > 0) {
+                sb.append(", ");
+            }
+
+            sb.append(nomeColuna);
+        }
+
+        sb.append(") VALUES (");
+
+        for (int i = 0; i < atributos.length; i++) {
+            if (i > 0) sb.append(", ");
+
+            sb.append("?");
+        }
+        sb.append(");");
+        String add = sb.toString();
+        //System.out.println(add);
+
+        PreparedStatement ps = null;
+
+        try {
+            ps = con.prepareStatement(add);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } 
+        return ps;
 	}
 
 	@Override
 	protected PreparedStatement getSqlSelectAll(Connection con, Object obj) {
-		System.out.println("TESTE1");
 		Class<?> cl = obj.getClass();
         StringBuilder sb = new StringBuilder();
         String nomeTabela;
-        System.out.println("TESTE2");
         if (cl.isAnnotationPresent(Tabela.class)) {
             nomeTabela = cl.getAnnotation(Tabela.class).value();
         } else {
             nomeTabela = cl.getSimpleName().toUpperCase();
         }
         sb.append("SELECT * FROM ").append(nomeTabela).append(";");
-
         String selectFrom = sb.toString();
-        System.out.println(selectFrom);
+        //System.out.println(selectFrom);
         PreparedStatement ps = null;
 
         try {
