eb38ffd9e61975d66bfe4a7e7cd5e26fc8ffd5b0
Eliel <elielmd>
2016-04-26 00:24:45 -0300

    V4.1 - Criando interface Dao!

---
+++ TrabalhoBimestre/src/br/eliel/abstrata/Dao.java
@@ -0,0 +1,13 @@
+package br.eliel.abstrata;
+
+import java.util.List;
+
+public interface Dao<T,K> {
+	
+	public void salvar(T t);
+	public T buscar(K k);
+	public void atualizar(T t);
+	public void excluir(K k);
+	public List<T> listarTodos();
+	
+}
